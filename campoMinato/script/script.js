dd899b1c6f7c4a269c307de8a25c99206767b991
Abderrahman El Mellalou <88928403+eabde@users.noreply.github.com>
2023-05-03 22:30:46 +0200

    Update script.js

--- campoMinato/script/script.js
+++ campoMinato/script/script.js
@@ -1,21 +1,18 @@
-class Campo {
-    constructor() {
-        this.size = 10;
-        this.nBombe = 5;
-        this.campo = [];
-    }
-
-    creaCampo() {
-        for (let i = 0; i < this.size; i++) {
-            for (let j = 0; j < this.size; j++) {
-                $('#campo').append('<div class="cella" onclick="cambia()"></div>');
-            }
-            $('#campo').append("<br>");
-        }
-    }
-}
- 
-$(document).ready(function() {
-   const campo = new Campo();
+$(document).ready(function () {
+    const campo = new CCampo();
     campo.creaCampo();
+
+    $('#gCampo').change(function () {
+        campo.size = $(this).val();
+        $('#campo').empty();
+        campo.creaCampo();
+    });
+    $('#nMine').change(function () {
+        campo.mine = $(this).val();
+        $('#campo').empty();
+        campo.creaCampo();
+    });
+    $('.cella').click(function () {
+        $(this).css('background-color', 'red');
+    });
 });
